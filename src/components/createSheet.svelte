8f7cd6249f0f1441ecc1c6b6ab83f6f3bb3aef94
Nico-L <nicolas@luchier.fr>
2022-01-28 10:20:27 +0000

    test netfunction

---
+++ src/components/createSheet.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Bouton from './ui/bouton.svelte'
+
+    async function createSheet() {
+        console.log('pog')
+        const url = `/netlify/functions/createGSheet?name=testSheetURL`;
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+</script>
+
+<Bouton on:action={createSheet}>Créer Sheet</Bouton>
