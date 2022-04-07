5c2b385e8653d4f662ba6e6165189ae75aaf538b
Nico-L <nicolas@luchier.fr>
2022-04-07 12:13:08 +0000

    bloub

--- netlify/functions/saveInscriptions/saveInscriptions.js
+++ netlify/functions/saveInscriptions/saveInscriptions.js
@@ -18,7 +18,7 @@ const handler = async (event) => {
       const campagne = await leFetch.json()
       const gSheetId = campagne[0].gSheetId
       const titreColonnes = campagne[0].titreColonnes
-      console.log('recup campagne OK', event.queryStringParameters)
+      console.log('recup campagne OK', event)
       const inscriptions = JSON.parse(event.queryStringParameters.inscriptions) || []
       const effacer = JSON.parse(event.queryStringParameters.effacer) || []
       console.log('creation doc start')
