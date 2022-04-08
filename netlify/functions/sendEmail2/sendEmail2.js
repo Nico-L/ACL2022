a0e13132addc8b89ae9af12412ab4cc3d645b418
Nico-L <nicolas@luchier.fr>
2022-04-08 14:50:24 +0000

    test email...

--- netlify/functions/sendEmail2/sendEmail2.js
+++ netlify/functions/sendEmail2/sendEmail2.js
@@ -5,33 +5,53 @@
  */
 
  const sendgrid = require('@sendgrid/mail');
- const handlebars = require('handlebars');
- const fs = require('fs');
+ /*const handlebars = require('handlebars');
+ const fs = require('fs');*/
 
  sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
+/*
 const fileName = "./netlify/functions/sendEmail/template/confirmationEmail.hbs"
 const mjmlTemplateFile = fs.readFileSync(fileName, 'utf8');
 const template = handlebars.compile(mjmlTemplateFile);
-let hbsHtml; 
+let hbsHtml; */
 
 
  const handler = async (event) => {
-      const msg = {
-                to: "nicolas@luchier.fr",
-                from: {
-                email: "nicolas@luchier.fr",
-                name: "ACL"
-                },
-                subject: "Votre inscription à l'ACL",
-                html: "bob ?"
-            }
-            
-            sendgrid.send(msg).then(() => {
-                console.log("ça marche ?");
-            }, (error) => {
-                console.log("erreur", error);    
-            });
+
+const mailjet = require('node-mailjet').connect(
+  process.env.MJ_APIKEY_PUBLIC,
+  process.env.MJ_APIKEY_PRIVATE
+)
+const request = mailjet.post('send', { version: 'v3.1' }).request({
+  Messages: [
+    {
+      From: {
+        Email: 'nicolas@luchier.fr',
+        Name: 'Me',
+      },
+      To: [
+        {
+          Email: 'nicolas@luchier.fr',
+          Name: 'You',
+        },
+      ],
+      Subject: 'My first Mailjet Email!',
+      TextPart: 'Greetings from Mailjet!',
+      HTMLPart:
+        '<h3>Dear passenger 1, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!</h3><br />May the delivery force be with you!',
+    },
+  ],
+})
+request
+  .then(result => {
+    console.log('success !')
+    console.log(result.body)
+  })
+  .catch(err => {
+    console.log('erreur :(')
+    console.log(err.statusCode)
+  })
     /*const dataEmail = JSON.parse(event.queryStringParameters.dataEmail) || null
     const email = event.queryStringParameters.email || null
     
@@ -48,7 +68,7 @@ let hbsHtml;
                 name: "ACL"
                 },
                 subject: "Votre inscription à l'ACL",
-                html: "bob ?"
+                html: hbsHtml
             }
             
             sendgrid.send(msg).then(() => {
@@ -57,7 +77,7 @@ let hbsHtml;
                 console.log("erreur", error);    
             });
         }
-    } */
+    }*/
     return {
         statusCode: 200,
         body: JSON.stringify({ retour: "ok"}),
