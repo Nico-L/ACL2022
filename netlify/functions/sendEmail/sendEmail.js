131c50cbe6a62e4245410bed008882073c428d68
Nico-L <nicolas@luchier.fr>
2022-04-08 14:06:08 +0000

    test mailjet

--- netlify/functions/sendEmail/sendEmail.js
+++ netlify/functions/sendEmail/sendEmail.js
@@ -19,24 +19,37 @@ let hbsHtml; */
 
  const handler = async (event) => {
 
-const msg = {
-  to: 'nicolas@luchier.fr',
-  from: 'nicolas@luchier.fr', // Use the email address or domain you verified above
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-console.log("message", process.env.SENDGRID_API_KEY)
-//ES6
-sendgrid
-  .send(msg)
-  .then(() => {console.log('hiop !')}, error => {
-    console.log(error);
-
-    if (error.response) {
-      console.log(error.response.body)
-    }
-  });
+const mailjet = require('node-mailjet').connect(
+  process.env.MJ_APIKEY_PUBLIC,
+  process.env.MJ_APIKEY_PRIVATE
+)
+const request = mailjet.post('send', { version: 'v3.1' }).request({
+  Messages: [
+    {
+      From: {
+        Email: 'nicolas@luchier.fr',
+        Name: 'Me',
+      },
+      To: [
+        {
+          Email: 'nicolas@luchier.fr',
+          Name: 'You',
+        },
+      ],
+      Subject: 'My first Mailjet Email!',
+      TextPart: 'Greetings from Mailjet!',
+      HTMLPart:
+        '<h3>Dear passenger 1, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!</h3><br />May the delivery force be with you!',
+    },
+  ],
+})
+request
+  .then(result => {
+    console.log(result.body)
+  })
+  .catch(err => {
+    console.log(err.statusCode)
+  })
     /*const dataEmail = JSON.parse(event.queryStringParameters.dataEmail) || null
     const email = event.queryStringParameters.email || null
     
