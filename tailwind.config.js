034302cd43d4bbb3892d8e5159d9b5cff1cd3539
Nico-L <nicolas@luchier.fr>
2022-07-05 06:22:56 +0000

    fix config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+    },
       borderWidth: {
         '6': '6px',
         '10': '10px',
@@ -108,21 +114,6 @@ module.exports = {
         800: "#2d3748",
         900: "#1a202c"
       },
-    screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
     }
   },
   plugins: [
