1e551d081a2345f93bce12acdf0ac76a3f2d11a7
Nico-L <nicolas@luchier.fr>
2024-01-15 21:04:54 +0000

    modif astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
+import keystatic from '@keystatic/astro'
+//import image from "@astrojs/image";
+
+//import react from "@astrojs/react";
+//import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
@@ -11,13 +15,14 @@ export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
   outDir: './dist/',
   srcDir: './src/',
   site: 'https://acl-sappey.netlify.app/',
-  integrations: [svelte(), tailwind(), image()],
+  integrations: [svelte(), tailwind()],
+  output: 'hybrid',
   vite: {
     server: {
       // configure vite for HMR with Gitpod
       hmr: process.env.GITPOD_WORKSPACE_URL ? {
         // removes the protocol and replaces it with the port we're connecting to
-        host: process.env.GITPOD_WORKSPACE_URL.replace('https://', '3000-'),
+        host: process.env.GITPOD_WORKSPACE_URL.replace('https://', '4321-'),
         protocol: 'wss',
         clientPort: 443
       } : true
